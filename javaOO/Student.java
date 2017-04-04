e7c85e8c3d9e77b499f4b36f8780e5f7aeea755f
joik1122 <joik1122@gmail.com>
2017-04-04 13:31:01 +0900

    1번 문제
    1번 문제

---
+++ javaOO/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+
+	String dclass;
+	int num;
+}
+
+ class out{
+	
+	 public static void main(String[] args){
+		 
+		 Student st = new Student();
+		 
+		 st.dclass = "컴퓨터 정보 학과";
+		 st.num = 20132752;
+		 
+		 System.out.println("학과 = "+st.dclass);
+		 System.out.println("학번 = "+st.num);
+	 }
+	
+}
